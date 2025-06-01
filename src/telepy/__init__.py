be2259142047c0fc91008fa89db195ff030563d8
changlehung <changlehung@gmail.com>
2025-06-02 03:21:18 +0800

    Add telepy __init__.py

---
+++ src/telepy/__init__.py
@@ -0,0 +1,58 @@
+"""."""
+
+from types import FrameType
+
+from . import _telepysys  # type: ignore
+
+__all__: list[str] = ["__version__", "current_frames", "current_stacks", "version"]
+
+__version__: str = _telepysys.__version__
+version: str = __version__
+
+
+def current_frames() -> dict[int, FrameType]:
+    """
+    Returns:
+        A dictionary of thread IDs to frame objects, representing
+        the current stack trace for each frame.
+    """
+    return _telepysys.current_frames()  # pragma: no cover
+
+
+def current_stacks() -> dict[int, list[str]]:
+    """
+    Returns:
+        A dictionary of thread IDs to lists of strings, representing
+        the current stack trace for each frame.
+    """
+    frames = _telepysys.current_frames()
+    res: dict[int, list[str]] = dict()
+
+    for tid, frame in frames.items():
+        stack: list[str] = []
+        while True:
+            co = frame.f_code
+            filename = co.co_filename
+            lineno = frame.f_lineno
+            qualname = co.co_qualname
+
+            stack.append(f"{filename}:{lineno} {qualname}")
+            if frame.f_back is None:
+                break
+            frame = frame.f_back
+        res[tid] = stack
+    return res
+
+
+def join_current_stacks(stack_dict: dict[int, list[str]]) -> dict[int, str]:
+    """
+    Args:
+        stack_dict:
+            dict of thread id to stack
+    Returns:
+        dict of thread id to pretty stack
+    """
+    res: dict[int, str] = {}
+    for f_id, stack in stack_dict.items():
+        res[f_id] = "\n".join(stack)
+    return res
