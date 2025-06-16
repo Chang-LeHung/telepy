4eb29291aee9460af5e5b8def946924614efe002
changlehung <changlehung@gmail.com>
2025-06-17 03:33:49 +0800

    Add logging util

---
+++ src/telepy/logging.py
@@ -0,0 +1,12 @@
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+
+def log_success_panel(content: str) -> None:
+    console.print(Panel(content, style="green", title="Info"))
+
+
+def log_error_panel(content: str) -> None:
+    console.print(Panel(content, style="red", title="Error"))
