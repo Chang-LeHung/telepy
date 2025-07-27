1ed519b2dd04060dccf611381dcac34fdebfda20
changlehung <changlehung@gmail.com>
2025-07-27 16:34:00 +0800

    Add install_monitor into telepy

--- src/telepy/__init__.py
+++ src/telepy/__init__.py
@@ -1,13 +1,18 @@
 """."""
 
+import threading
 from types import FrameType
 
 from . import _telepysys  # type: ignore
 from ._telepysys import sched_yield, unix_micro_time
+from .monitor import TelePyMonitor
 from .sampler import TelepySysAsyncSampler, TelepySysAsyncWorkerSampler, TelepySysSampler
+from .shell import TelePyShell
 from .thread import in_main_thread
 
 __all__: list[str] = [
+    "TelePyMonitor",
+    "TelePyShell",
     "TelepySysAsyncSampler",
     "TelepySysAsyncWorkerSampler",
     "TelepySysSampler",
@@ -15,6 +20,7 @@ __all__: list[str] = [
     "current_frames",
     "current_stacks",
     "in_main_thread",
+    "install_monitor",
     "sched_yield",
     "unix_micro_time",
     "version",
@@ -70,3 +76,32 @@ def join_current_stacks(stack_dict: dict[int, list[str]]) -> dict[int, str]:
     for f_id, stack in stack_dict.items():
         res[f_id] = "\n".join(stack)
     return res
+
+
+def install_monitor(
+    port: int = 8026, host: str = "127.0.0.1", log=True, in_thread: bool = False
+) -> TelePyMonitor:
+    """
+    Install a TelePy monitor to the current process. If you want to shutdown
+    the monitor gracefully, call minitor.shutdown() and monitor.close() then.
+    Args:
+        port (int):
+            port to listen on
+        host (str):
+            host to listen on
+        log (bool):
+            whether to log
+        in_thread (bool):
+            whether to run in the current thread
+    Returns:
+        TelePyMonitor: the monitor instance
+        Call monitor.close() to stop the monitor.
+    """
+    monitor = TelePyMonitor(port, host, log=log)
+    if in_thread:
+        monitor.run()
+    else:
+        t = threading.Thread(target=monitor.run)
+        t.daemon = True
+        t.start()
+    return monitor
